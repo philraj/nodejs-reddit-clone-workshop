d4fea8dd251b947413f73d273f940aa7ec78db69
Phil Rajchgot <tophil@outlook.com>
2016-02-12 21:02:46 +0000

    wrote data-model.txt

--- create.sql
+++ create.sql
@@ -2,16 +2,18 @@
 -- Posts: each post should have the URL of the post, a title, and a reference to the user who posted it
 -- Votes: each vote has a link to a user, a link to a post, and an up/down flag
 
+CREATE DATABASE reddit_clone;
+
 CREATE TABLE Users (
   id INT auto_increment primary key,
   email VARCHAR(255),
-  screenName VARCHAR(255),
+  username VARCHAR(255),
   password VARCHAR(40)
   createdAt TIMESTAMP,
   updatedAt TIMESTAMP
   );
   
-CREATE TABLE Posts (
+CREATE TABLE Content (
   id INT auto_increment primary key,
   url TEXT,
   title VARCHAR(255),
@@ -24,7 +26,7 @@ CREATE TABLE Votes (
   id INT auto_increment primary key,
   userId INT,
   postId INT,
-  type ENUM('up', 'down')
+  upVote BOOLEAN,
   createdAt TIMESTAMP,
   updatedAt TIMESTAMP
   );
---
+++ reddit-clone.js
@@ -0,0 +1,32 @@
+var Sequelize = require("sequelize");
+
+var db = new Sequelize('reddit_clone', 'philraj', null, {
+  dialect: 'mysql'
+});
+
+var User = db.define('user', {
+  username: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING // TODO: make the passwords more secure!
+});
+
+// Even though the content belongs to users, we will setup the userId relationship later
+var Content = db.define('content', {
+  url: Sequelize.STRING,
+  title: Sequelize.STRING
+});
+
+// Even though a vote has a link to user and content, we will setup the relationship later
+var Vote = db.define('vote', {
+  upVote: Sequelize.BOOLEAN
+});
+
+// User <-> Content relationship
+Content.belongsTo(User); // This will add a `setUser` function on content objects
+User.hasMany(Content); // This will add an `addContent` function on user objects
+
+// User <-> Vote <-> Content relationship
+User.belongsToMany(Content, {through: Vote, as: 'Upvotes'}); // This will add an `add`
+Content.belongsToMany(User, {through: Vote});
+
+db.sync(); // Only needs to be used once!
